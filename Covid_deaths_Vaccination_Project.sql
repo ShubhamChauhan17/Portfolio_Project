7529503dc0dbfe369d34e6f5aac244e83f9ab533
ShubhamChauhan17 <116322714+ShubhamChauhan17@users.noreply.github.com>
2022-11-04 16:50:42 +0530

    Update Covid_deaths_Vaccination_Project.sql

--- Covid_deaths_Vaccination_Project.sql
+++ Covid_deaths_Vaccination_Project.sql
@@ -121,8 +121,8 @@ Create View PercentPopulationVaccinated as
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
 --, (RollingPeopleVaccinated/population)*100
-From PortfolioProject..CovidDeaths dea
-Join PortfolioProject..CovidVaccinations vac
+From `kagglefirst-step1717.CovidProject.CovidDeaths` AS dea
+Join `kagglefirst-step1717.CovidProject.CovidVaccinations` AS vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null 
